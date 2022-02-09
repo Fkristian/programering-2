ac325a94b199c62c2b07d8a1f6dd1096ba971290
kristian <kfranzen@kth.se>
2022-02-09 12:25:55 +0100

    Update prime.ex

--- lib/prime.ex
+++ lib/prime.ex
@@ -4,37 +4,18 @@ defmodule Prime do
     list = Enum.to_list(2..n)
     pickOutPrime(list)
   end
+  def pickOutPrime([])do [] end
   def pickOutPrime([prime | tail])do
     tail = removeNonePrime(prime, tail)
+    [prime | pickOutPrime(tail)]
   end
-  def pickOutPrime(prime, [head | tail])do
-    case rem prime, head do
-      0 -> pickOutPrime(prime, [head | tail])
+  def removeNonePrime(prime, [])do [] end
+  def removeNonePrime(prime, [head | tail])do
+    case rem head, prime do
+      0 -> removeNonePrime(prime, tail)
+      _ -> [head | removeNonePrime(prime, tail)]
     end
   end
-
-
-  def pri1(n) do
-    [head | tail] = Enum.to_list(2..n)
-    pickOutPrime([head], tail, [])
-  end
-  def pickOutPrime(e, [], [])do e end
-  def pickOutPrime(e, [], n)do
-    [nextPrime | tail] = reverse(n)
-    pickOutPrime([nextPrime | e], tail, [])
-  end
-  def pickOutPrime([div | primes], [head | tail], n)do
-   case rem head, div do
-     0 -> pickOutPrime([div | primes], tail, n)
-     _ -> pickOutPrime([div | primes], tail, [head | n])
-   end
-  end
-
-
-
-
-
-
 
 
   def pri2(n) do
@@ -58,13 +39,21 @@ defmodule Prime do
 
   def pri3(n) do
     list = Enum.to_list(2..n)
-    stuff(list, [])
+    reverse(checkPrimeWithListPutFirst(list, []))
   end
 
+  def checkPrimeWithListPutFirst([head | tail], [])do checkPrimeWithListPutFirst(tail, [head]) end
 
+  def checkPrimeWithListPutFirst([], primes) do primes end
+  def checkPrimeWithListPutFirst([head | tail], prims)do checkPrimeWithListPutFirst(tail, ifNotDivWithPrimsPutFirst(head, prims, prims)) end
 
-
-
+  def ifNotDivWithPrimsPutFirst(check, [], prims)do [check | prims] end
+  def ifNotDivWithPrimsPutFirst(check, [head | tail], prims)do
+    case rem check, head do
+      0 -> prims
+      _ -> ifNotDivWithPrimsPutFirst(check , tail, prims)
+    end
+  end
 
 
 
